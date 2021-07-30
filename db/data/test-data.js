4fd952bfc3bc69b7c64333eb062df451f6538a9c
PaulOR26 <pj_orourke@outlook.com>
2021-07-30 16:21:56 +0100

    add test-data.js file

---
+++ db/data/test-data.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    breed_param: 'cockerspaniel',
+    breed: 'Cocker Spaniel',
+    temperament: 'Gentle and affectionate',
+    characteristics: 'Merry nature',
+    exercise_requirements: '1 hour +',
+    size: 'Medium',
+    dog_url:
+      'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/16105011/English-Cocker-Spaniel-Slide03.jpg',
+  },
+];
