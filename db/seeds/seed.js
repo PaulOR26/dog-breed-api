15afa4e444ebc71f7cdec6349d9c444a3232120c
PaulOR26 <pj_orourke@outlook.com>
2021-08-01 21:09:25 +0100

    amend createCollection to await promise

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -11,7 +11,7 @@ const seed = async (data) => {
     console.log('The collection did not already exist. No action necessary.');
   }
 
-  client.db().createCollection('breeds');
+  await client.db().createCollection('breeds');
   const collection = client.db().collection('breeds');
   await collection.insertMany(data);
 };
