ab11174bf9d8eb7e24be79442c6605805753802e
PaulOR26 <pj_orourke@outlook.com>
2021-08-01 20:28:09 +0100

    add try catch for if collection does not already exist

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -4,10 +4,14 @@ const seed = async (data) => {
   await client.connect();
 
   const dropCollection = client.db().collection('breeds');
-  dropCollection.drop();
+
+  try {
+    await dropCollection.drop();
+  } catch (err) {
+    console.log('The collection did not already exist. No action necessary.');
+  }
 
   client.db().createCollection('breeds');
-
   const collection = client.db().collection('breeds');
   await collection.insertMany(data);
 };
