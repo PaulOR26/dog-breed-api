e2f88f8e125f7b436a27ac908a9a709eb2b2f2d7
PaulOR26 <pj_orourke@outlook.com>
2021-08-03 10:54:28 +0100

    amend file structures

--- db/connection.js
+++ db/connection.js
@@ -5,12 +5,13 @@ const ENV = process.env.NODE_ENV || 'development';
 require('dotenv').config({
   path: path.resolve(__dirname, `../.env.${ENV}`),
 });
+// console.log('atlas', process.env.MONGO_ATLAS_URI);
 
-if (!process.env.MONGODB_URI) {
+if (!process.env.MONGO_ATLAS_URI) {
   throw new Error('MONGO_ATLAS_URI or DATABASE_URL not set');
 }
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGO_ATLAS_URI;
 
 const client = new MongoClient(uri);
 
