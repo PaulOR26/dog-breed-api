2b16f001d9c159bff3be02c52e76ec34e32dc75d
PaulOR26 <pj_orourke@outlook.com>
2021-08-03 10:53:32 +0100

    convert received base64 image to tensor

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const { getDogByBreed, getAllDogs } = require('./mvc/controllers');
+const cors = require('cors');
+
+const {
+  getDogByBreed,
+  getAllDogs,
+  getPrediction,
+} = require('./mvc/controllers');
 const {
   notFound,
   handleCustomerErrors,
@@ -8,8 +14,14 @@ const {
 
 const app = express();
 
+app.use(cors());
+
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb' }));
+
 app.get('/api/breeds', getAllDogs);
 app.get('/api/breeds/:breed', getDogByBreed);
+app.get('/api/photo', getPrediction);
 
 app.all('*', notFound);
 
