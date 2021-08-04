0920932204d9d0f31a82aa07277bbdacccefa408
PaulOR26 <pj_orourke@outlook.com>
2021-08-04 13:16:49 +0100

    update the process env when running seed:prod

--- listen.js
+++ listen.js
@@ -5,7 +5,3 @@ app.listen(PORT, (err) => {
   if (err) throw err;
   console.log(`listening on ${PORT}!`);
 });
-
-// load model and classifier in listen.js
-//import them in to model
-// base64 to tensor and run prediction
